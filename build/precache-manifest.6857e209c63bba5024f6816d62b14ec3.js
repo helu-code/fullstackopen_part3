b4067bf61965508df361fb252b2da516081e8837
heikkilumiaho <heikki.lumiaho@talenom.fi>
2019-08-12 16:08:59 +0300

    fronend

---
+++ build/precache-manifest.6857e209c63bba5024f6816d62b14ec3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19552f955e3caf35cacebbf7fba4138d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66da24170bd538591d9f",
+    "url": "/static/css/main.903f9c60.chunk.css"
+  },
+  {
+    "revision": "6a19a0beea039a280f26",
+    "url": "/static/js/2.3c62b441.chunk.js"
+  },
+  {
+    "revision": "66da24170bd538591d9f",
+    "url": "/static/js/main.515f618f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
